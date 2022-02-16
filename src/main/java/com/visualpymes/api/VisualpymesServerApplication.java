fc0f8a293bd5a2705274d551954f93944367956c
judithquine <judithquine@yahoo.es>
2022-02-16 18:09:14 +0100

    armando proyecto

---
+++ src/main/java/com/visualpymes/api/VisualpymesServerApplication.java
@@ -0,0 +1,13 @@
+package com.visualpymes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualpymesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisualpymesServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/visualpymes/api/VisualpymesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visualpymes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualpymesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
